10832afc85863bb03a05a6d0df2692a17a160181
sonam <tseringsonam34@hotmail.com>
2022-09-02 10:21:57 -0400

    divs for months

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let months = [
     'november',
     'december'
 ]
-let weekdays = [
+let weekDays = [
     'sunday',
     'monday',
     'tuesday',
@@ -23,3 +23,17 @@ let weekdays = [
     'saturday'
 ]
 
+const calendar = document.querySelector('.calendar');
+
+months.forEach((month, idx) => {
+	monthsHTML += `<div class="months month_${idx}">
+        <h3>${month}</h3>
+        <div class="week_days_container">
+            ${weekDays
+				.map(day => `<div class="week_days">${day}</div>`)
+				.join('')}
+        </div>
+        <div class="days_container"></div>
+    </div>`;
+});
+calendar.innerHTML = monthsHTML;
