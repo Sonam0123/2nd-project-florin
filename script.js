3c86304596fec8ca242dad8f2798932b4f404462
sonam <tseringsonam34@hotmail.com>
2022-09-02 10:14:20 -0400

    fontawesome emojis

---
+++ script.js
@@ -0,0 +1,25 @@
+let currentYear = 2022;
+let months = [
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december'
+]
+let weekdays = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday'
+]
+
